c833ce097f5304cefcee405a2c1aa31edfb65be1
Ryo Nakamura <r7kamura@gmail.com>
2022-05-13 05:44:59 +0900

    Replace github.token with GITHUB_TOKEN env

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ echo /vendor/ >> .git/info/exclude
 
 git config remote.origin.url "git@github.com:${GITHUB_REPOSITORY}.git"
 
-export GITHUB_ACCESS_TOKEN="$2"
+export GITHUB_ACCESS_TOKEN="${GITHUB_TOKEN}"
 
 rubocop_challenger go \
   --base-branch="$1" \
